983b5b41ff30332a8e675aae6e6d44378d1af7bf
Vojtech Kumpost <vojtech.kumpost@kit.edu>
2022-03-30 14:47:13 +0200

    Add Goodwin model

--- src/model_library.jl
+++ src/model_library.jl
@@ -1,10 +1,14 @@
 """
-`load_model(model_name::String, problem_type::String)`
+`load_model`
 
 Load a model from the library.
 
 **Arguments**
-- `model_name`: Name of a model.
+- `model_name`: Name of a model. Currently implemented models are:
+    - `amplitude-phase`
+    - `goodwin`
+    - `goodwin-2`
+    - `van-der-pol`
 
 **Optional Arguments**
 - `problem_type`: "ode", "sde", or "jump".
@@ -13,6 +17,12 @@ function load_model(model_name::String, problem_type::String="ode")
 
     if model_name == "amplitude-phase"
         model = _load_amplitude_phase(problem_type)
+    elseif model_name == "goodwin"
+        model = _load_goodwin(problem_type)
+    elseif model_name == "goodwin-2"
+        model = _load_goodwin_2(problem_type)
+    # elseif model_name == "van-der-pol"
+        # model = _load_van_der_pol(problem_type)
     else
         err = OscillatorPopulationError("`$model_name` is not in the model library!")
         throw(err)
@@ -29,7 +39,7 @@ end
 Load the amplitude-phase model.
 
 **References**
-- Tokuda, Isao T., et al. "Conceptual models of entrainment, jet lag, and
+- Isao T. Tokuda, et al. "Conceptual models of entrainment, jet lag, and
     seasonality." Frontiers in Physiology 11 (2020): 334.
 """
 function _load_amplitude_phase(problem_type)
@@ -95,6 +105,121 @@ function _load_amplitude_phase(problem_type)
     else
 
         msg = "Problem type `$problem_type` not implemented! Use `ode` or `sde`."
+        err = OscillatorPopulationError(msg)
+        throw(err)
+
+    end
+
+    model = Model(variable_names, parameter_names, problem, solver_algorithm,
+        solver_parameters, input, output)
+        
+    return model
+
+end
+
+
+"""
+`_load_goodwin`
+
+Load the Goodwin model.
+
+**References**
+- Didier Gonze and Peter Ruoff. "The Goodwin oscillator and its legacy." Acta
+    Biotheoretica 69.4 (2021): 857-874.
+"""
+function _load_goodwin(problem_type)
+
+    function model!(du, u, p, t)
+
+        # Variables
+        x, y, z = u
+        
+        # Parameters
+        K, n, a1, a2, a3, d1, d2, d3 = p
+        
+        # Equations
+        du[1] = a1 * K^n / (K^n + z^n) - d1 * x
+        du[2] = a2 * x - d2 * y
+        du[3] = a3 * y - d3 * z
+
+    end
+
+    # Common variables for all model types
+    tspan = (0.0, 100.0)
+    variable_names = ["x", "y", "z"]
+    parameter_names = ["K", "n", "a1", "a2", "a3", "d1", "d2", "d3"]
+    p = [1.0, 10.0, 5.0, 5.0, 5.0, 0.5, 0.5, 0.5]
+    u0 = [0.1, 0.1, 0.1]
+    input = (Matrix{Float64}(undef, 0, 0), "")
+    output = sol -> Matrix(sol[:, :]')
+        
+    if problem_type == "ode"
+        
+        # Create an ODE problem
+        problem = ODEProblem(model!, u0, tspan, p)
+    
+        # Set solver parameters
+        solver_algorithm = DP5()
+        solver_parameters = (saveat=0.01, reltol=1e-9, abstol=1e-9,)
+
+    else
+
+        msg = "Problem type `$problem_type` not implemented! Use `ode`."
+        err = OscillatorPopulationError(msg)
+        throw(err)
+
+    end
+
+    model = Model(variable_names, parameter_names, problem, solver_algorithm,
+        solver_parameters, input, output)
+        
+    return model
+
+end
+
+
+"""
+`_load_goodwin_2`
+
+Load the 2-equation Goodwin model.
+"""
+function _load_goodwin_2(problem_type)
+
+    function model!(du, u, p, t)
+
+        # Variables
+        x, y = u
+        
+        # Parameters
+        K, n, a1, a2, d1, d2 = p
+        
+        # Equations
+        du[1] = a1 * K^n / (K^n + y^n) - d1 * x
+        du[2] = a2 * x - d2 * y
+
+    end
+
+    # Common variables for all model types
+    tspan = (0.0, 100.0)
+    variable_names = ["x", "y"]
+    parameter_names = ["K", "n", "a1", "a2", "d1", "d2"]
+    p = [1.0, 10.0, 5.0, 5.0, 0.5, 0.5]
+    u0 = [0.1, 0.1]
+    input = (Matrix{Float64}(undef, 0, 0), "")
+    output = sol -> Matrix(sol[:, :]')
+        
+    if problem_type == "ode"
+        
+        # Create an ODE problem
+        problem = ODEProblem(model!, u0, tspan, p)
+    
+        # Set solver parameters
+        solver_algorithm = DP5()
+        solver_parameters = (saveat=0.01, reltol=1e-9, abstol=1e-9,)
+
+    else
+
+        msg = "Problem type `$problem_type` not implemented! Use `ode`."
         err = OscillatorPopulationError(msg)
         throw(err)
 
--- test/model_library_test.jl
+++ test/model_library_test.jl
@@ -42,3 +42,55 @@ end
     @test_throws OscillatorPopulationError load_model("amplitude-phase", "jump")
 
 end
+
+@testset "goodwin model" begin
+
+    # ode model
+    model = load_model("goodwin", "ode")
+    @test model.variable_names == ["x", "y", "z"]
+    @test model.parameter_names == ["K", "n", "a1", "a2", "a3", "d1", "d2", "d3"]
+    @test model.problem isa ODEProblem
+    @test model.problem.u0 == [0.1, 0.1, 0.1]
+    @test model.problem.tspan == (0.0, 100.0)
+    @test model.problem.p == [1.0, 10.0, 5.0, 5.0, 5.0, 0.5, 0.5, 0.5]
+    @test model.solver_algorithm == DP5()
+    @test model.solver_parameters == (saveat=0.01, reltol=1e-9, abstol=1e-9,)
+    @test isempty(model.input[1])
+    @test model.input[1] isa Matrix
+    @test isempty(model.input[2])
+    @test model.input[2] isa String
+    @test model.output isa Function
+
+    # sde model
+    @test_throws OscillatorPopulationError load_model("goodwin", "sde")
+
+    # jump model
+    @test_throws OscillatorPopulationError load_model("goodwin", "jump")
+    
+end
+
+@testset "goodwin-2 model" begin
+
+    # ode model
+    model = load_model("goodwin-2", "ode")
+    @test model.variable_names == ["x", "y"]
+    @test model.parameter_names == ["K", "n", "a1", "a2", "d1", "d2"]
+    @test model.problem isa ODEProblem
+    @test model.problem.u0 == [0.1, 0.1]
+    @test model.problem.tspan == (0.0, 100.0)
+    @test model.problem.p == [1.0, 10.0, 5.0, 5.0, 0.5, 0.5]
+    @test model.solver_algorithm == DP5()
+    @test model.solver_parameters == (saveat=0.01, reltol=1e-9, abstol=1e-9,)
+    @test isempty(model.input[1])
+    @test model.input[1] isa Matrix
+    @test isempty(model.input[2])
+    @test model.input[2] isa String
+    @test model.output isa Function
+
+    # sde model
+    @test_throws OscillatorPopulationError load_model("goodwin", "sde")
+
+    # jump model
+    @test_throws OscillatorPopulationError load_model("goodwin", "jump")
+    
+end
