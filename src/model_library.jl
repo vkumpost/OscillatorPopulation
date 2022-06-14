66fb4839b37b4d7a399f1d0b07ad28a40842517a
Vojtech Kumpost <vojtech.kumpost@kit.edu>
2022-06-14 14:18:59 +0200

    Add scaling for Kim-Forger model

--- src/model_library.jl
+++ src/model_library.jl
@@ -7,7 +7,7 @@ Load a model from the library.
 - `model_name`: Name of a model. Currently implemented models are:
     - `amplitude-phase`
     - `goodwin`
-    - `goodwin-general` (kwargs: `n_equations`)
+    - `goodwin-general` (kwargs: `n_equations`; default value is 3)
     - `van-der-pol`
     - `kim-forger`
 
@@ -400,17 +400,14 @@ function _load_kim_forger(problem_type)
 
         # Variables
         x, y, z = u
-        x = max(0, x)
-        y = max(0, y)
-        z = max(0, z)
         
         # Parameters
-        A, I = p
+        A, I, τ = p
         
         # Equations
-        du[1] = max(0, 1 - z/A) - x + I
-        du[2] = x - y
-        du[3] = y - z
+        du[1] = τ * max(0, 1 - z/A) - τ * x + I
+        du[2] = τ * x - τ * y
+        du[3] = τ * y - τ * z
 
     end
 
@@ -423,56 +420,106 @@ function _load_kim_forger(problem_type)
         z = max(0, z)
 
         # Parameters
-        A, I, σ = p
+        A, I, τ, σ = p
 
         # Equations
-        du[1, 1] = σ * sqrt(max(0, 1 - z/A))
-        du[1, 2] = σ * sqrt(x)
+        du[1, 1] = σ * sqrt(τ * max(0, 1 - z/A))
+        du[1, 2] = σ * sqrt(τ * x)
         du[1, 3] = σ * sqrt(I)
-        du[2, 4] = σ * sqrt(x)
-        du[2, 5] = σ * sqrt(y)
-        du[3, 6] = σ * sqrt(y)
-        du[3, 7] = σ * sqrt(z)
+        du[2, 4] = σ * sqrt(τ * x)
+        du[2, 5] = σ * sqrt(τ * y)
+        du[3, 6] = σ * sqrt(τ * y)
+        du[3, 7] = σ * sqrt(τ * z)
+
+    end
+
+    function jumps()
+
+        ix, iy, iz = 1:3
+        iA, iI, iτ, iΩ = 1:4
+
+        rate1(u, p, t) = p[iτ]*p[iΩ]*max(0, 1 - u[iz] / (p[iΩ] * p[iA]))
+        affect1!(integrator) = integrator.u[ix] += 1
+        jump1 = ConstantRateJump(rate1, affect1!)
+    
+        rate2(u, p, t) = p[iτ]*u[ix]
+        affect2!(integrator) = integrator.u[ix] -= 1
+        jump2 = ConstantRateJump(rate2, affect2!)
+    
+        rate3(u, p, t) = p[iτ]*u[ix]
+        affect3!(integrator) = integrator.u[iy] += 1
+        jump3 = ConstantRateJump(rate3, affect3!)
+    
+        rate4(u, p, t) = p[iτ]*u[iy]
+        affect4!(integrator) = integrator.u[iy] -= 1
+        jump4 = ConstantRateJump(rate4, affect4!)
+    
+        rate5(u, p, t) = p[iτ]*u[iy]
+        affect5!(integrator) = integrator.u[iz] += 1
+        jump5 = ConstantRateJump(rate5, affect5!)
+    
+        rate6(u, p, t) = p[iτ]*u[iz]
+        affect6!(integrator) = integrator.u[iz] -= 1
+        jump6 = ConstantRateJump(rate6, affect6!)
+    
+        rate7(u, p, t) = p[iΩ]*p[iI]
+        affect7!(integrator) = (integrator.u[iX] += 1)
+        jump7 = ConstantRateJump(rate7, affect7!)
+    
+        return jump1, jump2, jump3, jump4, jump5, jump6, jump7
 
     end
 
     # Common variables for all model types
     tspan = (0.0, 100.0)
     variable_names = ["x", "y", "z"]
-    parameter_names = ["A", "I"]
-    p = [0.1, 0.0]
+    parameter_names = ["A", "I", "τ"]
+    p = [0.1, 0.0, 3.66]
     u0 = [0.1, 0.1, 0.1]
     input = (Matrix{Float64}(undef, 0, 0), "")
     output = sol -> Matrix(sol[:, :]')
-    positive_domain_callback = PositiveDomain(u0; save=false)
         
     if problem_type == "ode"
         
         # Create an ODE problem
-        problem = ODEProblem(model!, u0, tspan, p; callback=positive_domain_callback)
+        problem = ODEProblem(model!, u0, tspan, p)
     
         # Set solver parameters
         solver_algorithm = DP5()
-        solver_parameters = (saveat=0.01, force_dtmin=true,)
+        solver_parameters = (saveat=0.01,)
 
     elseif problem_type == "sde"
 
         # Create an SDE model
         push!(parameter_names, "σ")
         push!(p, 0.1)
+            
         noise_rate_prototype = zeros(3, 7)
         problem = SDEProblem(model!, noise!, u0, tspan, p;
-            noise_rate_prototype=noise_rate_prototype,
-            callback=positive_domain_callback
+            noise_rate_prototype=noise_rate_prototype
         )
 
         # Set solver parameters
-        solver_algorithm = LambaEM()
-        solver_parameters = (saveat=0.01, force_dtmin=true,)
+        solver_algorithm = EM()
+        solver_parameters = (dt=0.001, saveat=0.01,)
+
+    elseif problem_type == "jump"
+
+        # Create a Jump model
+        push!(parameter_names, "Ω")
+        push!(p, 1000.0)
+        u0 .*= p[end]
+        discrete_problem = DiscreteProblem(u0, tspan, p)
+        problem = JumpProblem(discrete_problem, Direct(), jumps()...;
+            save_positions=(false, false))
+            
+        # Set solver parameters
+        solver_algorithm = SSAStepper()
+        solver_parameters = (saveat=0.01,)
 
     else
 
-        msg = "Problem type `$problem_type` not implemented! Use `ode` or `sde`."
+        msg = "Problem type `$problem_type` not implemented! Use `ode`, `sde`, or `jump`."
         err = OscillatorPopulationError(msg)
         throw(err)
 
