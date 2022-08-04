e84ad2c3a399be82f9c284d9bba1b49a256729b6
Vojtech Kumpost <vojtech.kumpost@kit.edu>
2022-08-04 11:18:03 +0200

    Correct test for create_simulation_function

--- test/metrics_test.jl
+++ test/metrics_test.jl
@@ -310,11 +310,12 @@ end
     set_solver!(model, saveat=1.0)
     fun = create_simulation_function(; transient=0.5, variable_x=2, variable_y=1)
     names = fun()
-    @test names == ["minimum", "maximum", "amplitude", "rms",
-        "winding_number", "autocorrelation",
-        "phase_coherence", "mean_phase",
+    @test names == ["minimum", "maximum", "amplitude", "rms", "winding_number",
+        "autocorrelation", "phase_coherence", "mean_phase",
         "phase_coherence_cxcorr", "mean_phase_cxcorr",
-        "phase_coherence_population", "collective_phase"]
+        "phase_coherence_population", "collective_phase",
+        "phase_coherence_population_cxcorr", "collective_phase_cxcorr"
+    ]
     
     properties = fun(model)
     @test properties[1] == 4
