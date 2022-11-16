594fdb06324a4e068185acdc9320fefa3364cc56
Vojtech Kumpost <vojtech.kumpost@kit.edu>
2022-11-16 14:39:31 +0100

    Organize tests for input/output functions

--- test/biolum_data_test.jl
+++ test/biolum_data_test.jl
@@ -208,11 +208,3 @@ end
     @test Vector(df_mean[:, "STD"]) ≈ [1, 3, 1, 2]
 
 end
-
-@testset "save_data, load_data, save_figure" begin
-
-    @test save_data isa Function
-    @test load_data isa Function
-    @test save_figure isa Function
-
-end
--- test/input_output_test.jl
+++ test/input_output_test.jl
@@ -1,7 +1,17 @@
-@testset "save_data, load_data, save_figure" begin
+@testset "save_data" begin
 
-    @test save_data isa Function
-    @test load_data isa Function
-    @test save_figure isa Function
+    @test hasmethod(save_data, Tuple{DataFrame, Any}, (:force,))
+
+end
+
+@testset "load_data" begin
+
+    @test hasmethod(load_data, Tuple{Any})
+
+end
+
+@testset "save_figure" begin
+
+    @test hasmethod(save_figure, Tuple{Any, Any}, (:force,))
 
 end
